3cc57c66776452ade77aa7b4a523c56ffdd2c0e3
Inna Akhmedova <inna2001.a@gmail.com>
2024-10-19 20:31:05 +0200

    pick ups and platform

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,10 +12,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	# Get the input direction and handle movement.
+	var direction := 0.0
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
+		direction = -1.0
+	elif Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
+		direction = 1.0
+	
+	# Apply movement or deceleration.
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
